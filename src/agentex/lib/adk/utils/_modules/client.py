5d8415e216ff65c047a818c9cede60bd40d34efb
Anish Agrawal <anishagrawal2003@gmail.com>
2025-07-26 21:02:23 -0700

    default headers

--- src/agentex/lib/adk/utils/_modules/client.py
+++ src/agentex/lib/adk/utils/_modules/client.py
@@ -7,6 +7,6 @@ from agentex.lib.environment_variables import refreshed_environment_variables
 def create_async_agentex_client(**kwargs):
     agent_id = refreshed_environment_variables.AGENT_ID
     default_headers = {
-        "x-agentex-identity": agent_id
+        "x-agent-identity": agent_id
     }
     return AsyncAgentex(default_headers=default_headers, **kwargs)
