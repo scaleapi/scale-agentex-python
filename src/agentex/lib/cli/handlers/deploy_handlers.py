0289d95143cc4c0e58a2abdcc8333623d0d15afc
Felix Su <felix.su@scale.com>
2025-09-15 17:36:56 -0700

    bump helm version

--- src/agentex/lib/cli/handlers/deploy_handlers.py
+++ src/agentex/lib/cli/handlers/deploy_handlers.py
@@ -22,7 +22,7 @@ logger = make_logger(__name__)
 console = Console()
 
 TEMPORAL_WORKER_KEY = "temporal-worker"
-AGENTEX_AGENTS_HELM_CHART_VERSION = "0.1.6-v1-beta"
+AGENTEX_AGENTS_HELM_CHART_VERSION = "0.1.7-v2-beta"
 
 
 class InputDeployOverrides(BaseModel):
