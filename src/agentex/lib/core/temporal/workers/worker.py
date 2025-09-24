65d6595af316cff5dbdc2e95c0a2c74eda140400
Prassanna Ravishankar <prassanna.ravishankar@scale.com>
2025-09-24 17:25:44 +0100

    remove default

--- src/agentex/lib/core/temporal/workers/worker.py
+++ src/agentex/lib/core/temporal/workers/worker.py
@@ -89,7 +89,7 @@ class AgentexWorker:
         task_queue,
         max_workers: int = 10,
         max_concurrent_activities: int = 10,
-        health_check_port: int = int(os.environ.get("HEALTH_CHECK_PORT", "80")),
+        health_check_port: int = int(os.environ.get("HEALTH_CHECK_PORT")),
     ):
         self.task_queue = task_queue
         self.activity_handles = []
