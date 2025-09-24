226d6fa2c96609880be5ab145f52fd58c34caa6d
Prassanna Ravishankar <prassanna.ravishankar@scale.com>
2025-09-24 15:43:10 +0100

    remove rye + all in on uv

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,8 @@ import nox
 
 @nox.session(reuse_venv=True, name="test-pydantic-v1")
 def test_pydantic_v1(session: nox.Session) -> None:
-    session.install("-r", "requirements-dev.lock")
+    # Install dependencies via uv
+    session.run("uv", "sync", "--group", "dev", external=True)
     session.install("pydantic<2")
 
     session.run("pytest", "--showlocals", "--ignore=tests/functional", *session.posargs)
